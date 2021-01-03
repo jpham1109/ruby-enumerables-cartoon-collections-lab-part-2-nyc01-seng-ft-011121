84ed1a7e4fc5e1701d3b83aff4db2167f786d7e1
Jamie Lawrence-Hurt <jpham1109@gmail.com>
2021-01-03 21:35:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,8 +28,6 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find do |element|
     element == valid_calls
-    if element != valid_calls
-      return nil
     end
   end
 end
